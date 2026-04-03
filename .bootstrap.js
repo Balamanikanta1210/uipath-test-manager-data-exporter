8840690768e49ecebd0c87748589a23d9ae11aeb
Nucleus <nucleus-bot@uipath.com>
2026-04-03 08:37:54 +0000

    chore: Update bootstrap script with latest commands

--- .bootstrap.js
+++ .bootstrap.js
@@ -77,7 +77,9 @@ function updateWranglerJsonc() {
 }
 
 function runSetupCommands() {
-    const commands = [];
+    const commands = [
+    "bun add exceljs@^4.4.0"
+];
     
     if (commands.length === 0) {
         console.log('⊘ No setup commands to run');
